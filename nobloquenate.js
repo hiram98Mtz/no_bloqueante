9e208e025a5ad3ff170d23b067a9683ab085f002
hiram98Mtz <hiram98ch@gmail.com>
2024-03-06 09:28:38 -0600

    primer commit no bloqueante json

---
+++ nobloquenate.js
@@ -0,0 +1,10 @@
+'use strict'
+setTimeout(function(){
+console-log('Tarea 1');
+}, 3000); 
+setTimeout(function(){
+console-log('Tarea 2');
+}, 20000); 
+setTimeout(function(){
+console-log('Tarea 3');
+}, 10000); 
